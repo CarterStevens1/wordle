c6a4fdf3954c10f1e84ab452641fac1cc959f645
Carter Stevens <carter@forthwithlife.co.uk>
2025-08-07 13:41:05 +0100

    add csrf token

--- resources/views/wordle-game.blade.php
+++ resources/views/wordle-game.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Simple Wordle</title>
     @vite(['resources/css/app.css'])
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
 </head>
 
